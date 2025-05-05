25e89cb6f478e4589656cfb48983857a523f8eb5
Ivy <125405494+IvyHirosawa@users.noreply.github.com>
2025-05-05 11:42:07 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -16,9 +16,12 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-11-30T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivo2 = new Date("2026-01-15T00:00:00");
+const tempoObjetivo3 = new Date("2025-12-18T00:00:00");
+const tempoObjetivo4 = new Date("2025-11-29T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -31,6 +34,22 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
- 
- return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    if (tempoFinal > 0) {
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    } else {
+        return "Prazo Finalizado";
+    }
 }
+
+function atualizaCronometro() {
+    for (let i = 0; i < contadores.length; i++) {
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+
+function comecaCronometro() {
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+
+//comecaCronometro();
